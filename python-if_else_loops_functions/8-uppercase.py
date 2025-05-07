5142eb190d1fb6ef3e7f3b3696f26d1b91976099
josniel <josnielramos@gmail.com>
2025-05-07 19:36:45 -0400

    8-uppercase.py test fix with print indent

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,4 @@ def uppercase(str):
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             print("{}".format(chr(ord(c) - 32)), end="")
-        # else:
-        #     print("{}".format(c), end="")
-            print()
+    print()
