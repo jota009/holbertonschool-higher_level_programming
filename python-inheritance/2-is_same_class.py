d75388237e053810adfcc5352e30d8570f8dfa40
josniel <josnielramos@gmail.com>
2025-05-26 19:49:07 -0400

    iskinfofclass commit

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,7 +3,7 @@
 
 
 def is_same_class(obj, a_class):
-    """Return True if ibj is exactly an instance of a_class.
+    """Return True if obj is exactly an instance of a_class.
 
     Args:
         obj: The object to test.
