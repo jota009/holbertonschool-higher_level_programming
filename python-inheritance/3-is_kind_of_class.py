8ded484255f3afb992404540c075473058837d08
josniel <josnielramos@gmail.com>
2025-05-26 19:48:22 -0400

    3-iskinfofclass first commit

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module defines is_kind_of_class function."""
+
+def is_kind_of_class(obj, a_class):
+    """Returns True if the obj is an instance of
+    or if obj is an instance of inherited class.
+
+    Args:
+        obj: The object to test.
+        a_class: The class to match againts.
+
+    Returns:
+       True if obj is an instance of a_class or its subclass,
+       otherwise False.
+    """
+    return isinstance(obj, a_class)
+
+
