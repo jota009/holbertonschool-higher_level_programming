9519bd4d2e45fd2cca43702129bc6d565a1c7258
josniel <josnielramos@gmail.com>
2025-05-27 13:06:55 -0400

    7-base_geometry.py first commit

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module that defines theh Basegeometry class."""
+
+
+class BaseGeometry:
+    """Base class for geometry objects."""
+
+    def area(self):
+        """Compute the area of the geometry.
+
+        Raises:
+            Exception: always, since this method must be
+                overridden by subclasses.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate that a given value is a positive integer.
+
+        Args:
+            name(str): Identifier used in error messages.
+            value(any): The value to validate.
+
+        Raises:
+            TypeError: if 'value' is not an integer.
+            ValueError: if 'value' is <= 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
