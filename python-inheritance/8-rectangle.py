3684c681c602e086d20c3b58c66a92e54b28a031
josniel <josnielramos@gmail.com>
2025-05-27 13:56:15 -0400

    base_geometry tsts files txt fix

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -29,3 +29,5 @@ class BaseGeometry:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
