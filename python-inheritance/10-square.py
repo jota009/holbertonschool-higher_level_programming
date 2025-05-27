a7a55aec6d236f8dd86c7474759d15c88d3a282a
josniel <josnielramos@gmail.com>
2025-05-27 14:49:29 -0400

    10-square.py first commit

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module that defines a subclass of Rectangle(from 9-rectangle.py)."""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square, defined by its size."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The lenght of the square's side.
+
+        Requirements:
+        - Validate 'size' as a positive integer uing integer_validation()
+        - Store it as a private attribute (no getter/setter)
+        """
+        self.integer_validator("size", size)
+
+        self.__size = size
+
+    def area(self):
+        """Compute and return the area of the square."""
+        return self.__size * self.__size
