565dabbcdedf3de397908cfccc0147432178e6eb
josniel <josnielramos@gmail.com>
2025-06-02 20:08:27 -0400

    3-json first commit

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module: 3_to_json_string.
+"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON string representation of my_obj.
+
+    Args:
+        my_obj: The Python object to convert (eg. dict, list, string, etc.)
+
+    Returns:
+        str: JSON string representation of my_obj.
+
+    Example:
+        >>> to_json_string({"a": 1, "b": 2})
+        '{"a": 1, "b": 2}'
+    """
+    return json.dumps(my_obj)
