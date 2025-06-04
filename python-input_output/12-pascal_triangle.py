133fe8172b6949a44ff9a5249b68d95a6995c0fb
josniel <josnielramos@gmail.com>
2025-06-03 22:04:37 -0400

    12-pascal first commit

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing
+    Pascal's triangle of n.
+
+    Args:
+        n (int): The number of rows to generate.
+
+    Returns:
+        list: Pascal's tringle as a list of lists.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for row_num in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]
+        for i in range(1, len(prev_row)):
+            new_row.append(prev_row[i - 1] + prev_row[i])
+        new_row.append(1)
+        triangle.append(new_row)
+    return triangle
