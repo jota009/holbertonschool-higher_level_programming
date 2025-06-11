056876569d56161825ef2d1b53287e44cc6a04f2
josniel <josnielramos@gmail.com>
2025-06-11 16:19:30 -0400

    tasks-2 fetch fix

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 
 
 def fetch_and_print_posts():
@@ -14,3 +15,23 @@ def fetch_and_print_posts():
         data = response.json()
         for post in data:
             print(post["title"])
+
+def fetch_and_save_posts():
+    """
+    Fetch all posts from JSONPlaceholder and save selected fields
+    (id, title, body) into posts.csv.
+    """
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    if response.status_code == 200:
+        data = response.json()
+
+        posts_list = [
+            {"id": post["id"], "title": post["title"], "body": post["body"]}
+            for post in data:
+        ]
+
+        with open("posts.csv", mode="w", newline="") as csvfile:
+            fieldnames = ["id", "title", "body"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(posts_list)
