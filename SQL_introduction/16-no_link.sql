52ca1743046c56756de17bdf346c6059391d38fa
jota009 <josnielramos@gmail.com>
2025-06-25 03:45:53 +0000

    task 16 frist commit

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,8 @@
+-- Task 16: List score and name for rows where name is not empty, ordered by score desc
+SELECT
+    score,
+    name
+FROM second_table
+WHERE name is not NULL
+    AND name <> ''
+ORDERED BY score DESC;
