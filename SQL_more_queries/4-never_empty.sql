9c5610ad5f9dc67819a459481df3452c9eb2d175
jota009 <josnielramos@gmail.com>
2025-06-27 04:12:22 +0000

    task 4 ouput fix

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
--- Task 4: Create if_not_null with id defaulting to 1 and a name column
-CREATE IF NOT EXISTS id_not_null (id INT NOT NULL DEFAULT 1, name VARCHAR(256));
+-- Task 4: Create id_not_null with id defaulting to 1 and a name column
+CREATE TABLE IF NOT EXISTS id_not_null (id INT NOT NULL DEFAULT 1, name VARCHAR(256));
