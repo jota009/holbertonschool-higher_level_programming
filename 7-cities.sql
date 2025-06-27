81869b1ea346112aabcd6d707b534381ba059f72
jota009 <josnielramos@gmail.com>
2025-06-27 04:28:15 +0000

    task 7 first commit

---
+++ 7-cities.sql
@@ -0,0 +1,8 @@
+-- Task 7: Create hbtn_0d_usa DB and cities tables with a foreign-key to states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
+);
