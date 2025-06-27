de9549bea87c90515753d0c0096fa8472186391c
jota009 <josnielramos@gmail.com>
2025-06-27 05:07:25 +0000

    task 10 output fix

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -4,7 +4,7 @@ SELECT
     tv_shows_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres
-    ON tv_shows.id tv_shows_genres.tv_show_id
+    ON tv_shows.id = tv_shows_genres.tv_show_id
 ORDER BY
     tv_shows.title ASC,
     tv_shows_genres.genre_id ASC;
