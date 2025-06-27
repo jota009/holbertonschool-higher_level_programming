5397e15948e90f3905029e116d46a018a0d0befc
jota009 <josnielramos@gmail.com>
2025-06-27 05:38:45 +0000

    task 12 fix ouput

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,4 @@
--- Task 11: List every show title and its genre_id (NULL if none)
+-- Task 12: List every show title and its genre_id (NULL if none)
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
