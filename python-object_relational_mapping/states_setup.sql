7ee374d2c3906eab9f7767c99f438ebc01e95387
josniel <josnielramos@gmail.com>
2025-06-27 19:56:29 -0400

    task2 output local test

---
+++ python-object_relational_mapping/states_setup.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
+USE hbtn_0e_0_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+  id   INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(128) NOT NULL
+);
+
+INSERT INTO states (name) VALUES
+  ('California'),
+  ('Arizona'),
+  ('Texas'),
+  ('New York'),
+  ('Nevada');
