e941f10c5998a63da83ea16fb5cdf2e4e27e1288
josniel <josnielramos@gmail.com>
2025-07-12 10:59:20 -0400

    task 11 c mmit

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+if (args.length < 2) {
+  console.log(0);
+} else {
+  const nums = args.map(Number);
+  nums.sort((a, b) => b - a);
+  console.log(nums[1]);
+}
